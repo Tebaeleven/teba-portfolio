9f654ae3c3ebda6bdad35334be031b7e383db1aa
nano <nano@nanonoMacBook-Air.local>
2023-05-21 21:26:21 +0900

    aboutページの追加

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,16 @@ import Layout from "@/components/layout";
 export default function About() {
     return (
         <>
-            
+            <div className="bg-white py-6 sm:py-8 lg:py-12">
+                <div className="mx-auto max-w-screen-lg px-4 md:px-8">
+                    <h1 className="mb-4 text-4xl font-bold text-gray-800">
+                        Teba_eleven
+                    </h1>
+                    <p className="mb-6 text-gray-800 ">
+                        普段はプログラミングをしています。
+                    </p>
+                </div>
+            </div>
         </>
-    )
+    );
 }
