a5ebc2565f4cb3960fe4bc42f98e35ae350f090f
nano <nano@nanonoMacBook-Air.local>
2023-05-21 22:24:01 +0900

    aboutページにmdxを読み込めるように

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,28 @@
 import Layout from "@/components/layout";
+import { postsFileNames, postsPath } from "@/utils/mdxUtils";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import mdxStyles from "@/components/mdx/mdx.module.css"
 
-export default function About() {
+export default function About({ source }) {
     return (
         <>
             <div className="bg-white py-6 sm:py-8 lg:py-12">
                 <div className="mx-auto max-w-screen-lg px-4 md:px-8">
-                    <h1 className="mb-4 text-4xl font-bold text-gray-800">
-                        Teba_eleven
-                    </h1>
-                    <p className="mb-6 text-gray-800 ">
-                        普段はプログラミングをしています。
-                    </p>
+                    <div className={mdxStyles.mdx}>
+                        <MDXRemote {...source}></MDXRemote>
+                    </div>
                 </div>
             </div>
         </>
     );
+}
+
+export async function getStaticProps() {
+    const content = fs.readFileSync(path.join(postsPath, "pages/about.mdx"));
+    const mdxSource = await serialize(content);
+    return { props: { source: mdxSource } };
 }
---
+++ utils/mdxUtils.tsx
@@ -0,0 +1,9 @@
+//nodejsに標準搭載されているpathとfsをimport
+import path from "path";
+import fs from "fs";
+//mdxが入っているフォルダのpath
+export const postsPath = path.join(process.cwd(), "mdxPosts");
+//postsPathmdx形式のファイルを全て取得する
+export const postsFileNames = fs
+    .readdirSync(postsPath)
+    .filter((fileName) => /\.mdx$/.test(fileName));
