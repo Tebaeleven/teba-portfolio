eb070bd8f0efb9711667fb6eeeee9f19491a415e
nano <nano@nanonoMacBook-Air.local>
2023-05-23 13:38:54 +0900

    worksのcardのリンクを修正

--- components/works/WorksCard.tsx
+++ components/works/WorksCard.tsx
@@ -11,7 +11,7 @@ export default function WorksCard({ post }) {
                     />
                 </a>
                 <div className="p-5">
-                    <Link href={"works/" + post.slug}>
+                    <Link href={"/works/" + post.slug}>
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
                             {post.frontmatter.title}
                         </h5>
@@ -37,7 +37,7 @@ export default function WorksCard({ post }) {
                         {post.frontmatter.desc}
                     </p>
                     <Link
-                        href={"works/" + post.slug}
+                        href={"/works/" + post.slug}
                         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         Read more
