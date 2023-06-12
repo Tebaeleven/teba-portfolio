89255a2d26838bcb5ec3e1480e5377c7b04bd53d
nano <nano@nanonoMacBook-Air.local>
2023-06-12 22:31:19 +0900

    Activitiesのurlをtwittericonに変更

--- components/Activities/Activities.tsx
+++ components/Activities/Activities.tsx
@@ -1,7 +1,8 @@
 import React, { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 import LiIcon from "@/components/Lilcon";
-const Details = ({ title, company, link, time, address, event }) => {
+import Link from "next/link";
+const Details = ({ title, company, link, time, address, event, twitter }) => {
     const ref = useRef(null);
 
     return (
@@ -29,6 +30,24 @@ const Details = ({ title, company, link, time, address, event }) => {
                     {time} | {address}
                 </span>
                 <p className="font-medium w-full">{event}</p>
+                {twitter && (
+                    <Link
+                        href={`${twitter}`}
+                        target="_blank"
+                        className="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
+                    >
+                        <svg
+                            className="h-5 w-5"
+                            width={24}
+                            height={24}
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                        </svg>
+                    </Link>
+                )}
             </motion.div>
         </li>
     );
@@ -60,7 +79,8 @@ export default function ActivitiesList() {
                         time="2023/06/11 15:00~18:00"
                         address="オフライン"
                         event="エンジニアカフェで開催された未踏福岡コンソーシアムの説明会に参加してきました。
-                        https://twitter.com/Tebasaki_lab/status/1667849734718111744?s=20"
+                        "
+                        twitter="https://twitter.com/Tebasaki_lab/status/1667849734718111744?s=20"
                     />
                     <Details
                         title="CoderDojo香椎第48回"
@@ -69,7 +89,8 @@ export default function ActivitiesList() {
                         time="2023/06/11 9:00~12:00"
                         address="オフライン"
                         event="並木スクエアで開催されたCoderDojo香椎にメンターとして参加してきました。
-                        https://twitter.com/Tebasaki_lab/status/1667703968242475008?s=20"
+                        "
+                        twitter="https://twitter.com/Tebasaki_lab/status/1667703968242475008?s=20"
                     />
                 </ul>
             </div>
